1072f7101657497472a216a64bfe3989ed1fda10
Andrii Vasyliev <andriivasyliev2@gmail.com>
2024-12-09 21:46:24 +0100

    Preparing to create a login and register an account

--- index.php
+++ index.php
@@ -29,7 +29,10 @@
 							<a href="#" class="nav__link">Kontakt</a>
 						</li>
 						<li class="nav__item">
-							<a href="#" class="nav__link nav__link--login">Zaloguj</a>
+							<a href="#" class="nav__link nav__link--login">Rejestracja</a>
+						</li>
+						<li class="nav__item">
+							<a href="#" class="nav__link nav__link--login">logowanie</a>
 						</li>
 					</ul>
 				</nav>
